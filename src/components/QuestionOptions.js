b063f37266be5869cf5d633aac6ad8dfa099efc9
Jessy Baki <bakijessy@gmail.com>
2023-09-17 07:21:37 +0000

    Add a button but I will update onClick event later

--- src/components/QuestionOptions.js
+++ src/components/QuestionOptions.js
@@ -2,29 +2,33 @@ function QuestionOptions({ question, dispatch, userAnswer }) {
     const hasAnswered = userAnswer !== null;
 
     return (
-        <ul className="options">
-            {question.options.map((option, index) => (
-                <button
-                    className={`btn btn-option ${userAnswer === index ? "answer" : ""} 
+        <>
+            <ul className="options">
+                {question.options.map((option, index) => (
+                    <button
+                        className={`btn btn-option ${userAnswer === index ? "answer" : ""} 
                     ${hasAnswered
-                            ? index === question.correctOption
-                                ? "correct"
-                                : "wrong"
-                            : ""
-                        }`
-                    }
-                    disabled={hasAnswered ? true : false}
-                    onClick={() => dispatch({
-                        type: "selectedAnswer",
-                        payload: index
-                    })}
-                    key={option}
-                >
-                    {option}
-                </button>
-            ))
-            }
-        </ul >
+                                ? index === question.correctOption
+                                    ? "correct"
+                                    : "wrong"
+                                : ""
+                            }`
+                        }
+                        disabled={hasAnswered ? true : false}
+                        onClick={() => dispatch({
+                            type: "selectedAnswer",
+                            payload: index
+                        })}
+                        key={option}
+                    >
+                        {option}
+                    </button>
+                ))
+                }
+            </ul >
+
+            <button className="btn" onClick={() => dispatch({})}>Next</button>
+        </>
     );
 }
 
