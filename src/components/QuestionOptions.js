b20ca93fe5198ac9537f10de3f12f879a67d1451
Jessy Baki <bakijessy@gmail.com>
2023-09-17 06:58:38 +0000

    Add a variable to check if user selected an answer

--- src/components/QuestionOptions.js
+++ src/components/QuestionOptions.js
@@ -1,10 +1,15 @@
-function QuestionOptions({ question, dispatch, usserAnswer }) {
+function QuestionOptions({ question, dispatch, userAnswer }) {
+    const hasAnswered = userAnswer !== null;
+
     return (
         <ul className="options">
             {question.options.map((option, index) => (
                 <button
                     className="btn btn-option"
-                    onClick={() => dispatch({ type: "selectedAnswer", payload: index })}
+                    onClick={() => dispatch({
+                        type: "selectedAnswer",
+                        payload: index
+                    })}
                     key={option}
                 >
                     {option}
