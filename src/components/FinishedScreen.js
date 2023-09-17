b48ebdc3e6765eb04f99adf58fe6ab76073507f4
Jessy Baki <bakijessy@gmail.com>
2023-09-17 08:59:07 +0000

    COmpute the score of user at the end of the quiz

--- src/components/FinishedScreen.js
+++ src/components/FinishedScreen.js
@@ -1,7 +1,9 @@
-function FinishedScreen() {
-    return (
-        <div>
+function FinishedScreen({ points, maxPossiblePoints }) {
+    const result = (points / maxPossiblePoints) * 100;
 
+    return (
+        <div className="result">
+            <p>You scored <strong>{points}</strong> out of {maxPossiblePoints} points ({Math.ceil(result)}%)</p>
         </div>
     );
 }
