8a0b6a2d159208257ebc8e2e7d4ef6aa47fa1a76
Jessy Baki <bakijessy@gmail.com>
2023-09-17 05:56:41 +0000

    Add a class to button in StartScreen component

--- src/StartScreen.js
+++ src/StartScreen.js
@@ -3,7 +3,7 @@ function StartScreen({ numOfQuestions }) {
         <div className="start">
             <h2>Welcome to The React Quiz!</h2>
             <h3>{numOfQuestions} questions to test your React mastery</h3>
-            <button className="btn">Let's Start!</button>
+            <button className="btn btn-ui">Let's Start!</button>
         </div>
     );
 }
