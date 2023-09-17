8c7aee8a6cd5c81ec974d870ebad489d5dbdff2e
Jessy Baki <bakijessy@gmail.com>
2023-09-17 10:23:59 +0000

    Add changes formatting of timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,7 +1,8 @@
 import { useEffect } from "react";
 
 function Timer({ dispatch, secondsRemaining }) {
-
+    const mins = Math.floor(secondsRemaining / 60);
+    const secs = secondsRemaining % 60;
 
     useEffect(function () {
         const id = setInterval(function () {
@@ -16,7 +17,9 @@ function Timer({ dispatch, secondsRemaining }) {
     );
 
     return (
-        <div className="timer">{secondsRemaining}</div>
+        <div className="timer">
+            {mins < 10 && "0"}{mins}:{secs < 10 && "0"}{secs}
+        </div>
     );
 }
 
